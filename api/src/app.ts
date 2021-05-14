fdf3de0505f928f88979bc189e0a93cf3568edb4
Dennis Chacko <39276940+dvchacko@users.noreply.github.com>
2021-05-14 06:14:09 -0500

    use google/gts to lint the project
    * refactor to use google gts for typescript style
    
    * fix failing ci tests

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,33 +1,37 @@
-import { Server } from "http";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { Context } from "aws-lambda";
+/* eslint @typescript-eslint/no-var-requires: "off" */
+import { Server } from 'http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Context } from 'aws-lambda';
 import * as serverlessExpress from 'aws-serverless-express';
-import express from 'express';
-import {ExpressAdapter} from '@nestjs/platform-express';
-
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 let lambdaProxy: Server;
 
 async function bootstrap() {
-    const expressServer = express();
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressServer));
-    await nestApp.init();
+  const expressServer = express();
+  const nestApp = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressServer),
+  );
+  await nestApp.init();
 
-    return serverlessExpress.createServer(expressServer);
+  return serverlessExpress.createServer(expressServer);
 }
 
-export const handler = async (event: any, context: Context) => { 
-  // Lambda Cold start: if warm, use cached server 
-  if (!lambdaProxy) { 
-    lambdaProxy = await bootstrap(); 
-  } 
-  try { 
-    return serverlessExpress.proxy(lambdaProxy, event, context, 'PROMISE').promise; 
-  } catch (e) { 
-    return { 
-      statusCode: 500, 
-      body: e.message 
-    } 
-  } 
+export const handler = async (event: any, context: Context) => {
+  // Lambda Cold start: if warm, use cached server
+  if (!lambdaProxy) {
+    lambdaProxy = await bootstrap();
+  }
+  try {
+    return serverlessExpress.proxy(lambdaProxy, event, context, 'PROMISE')
+      .promise;
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
 };
--- api/webpack.config.ts
+++ api/webpack.config.ts
@@ -1,6 +1,6 @@
-import { resolve } from 'path'; 
-import { Configuration, IgnorePlugin } from 'webpack'; 
-import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'; 
+/* eslint @typescript-eslint/no-var-requires: "off" */
+import { resolve } from 'path';
+import { Configuration, IgnorePlugin } from 'webpack';
 const ZipPlugin = require('zip-webpack-plugin');
 
 const config: Configuration = {
@@ -8,22 +8,22 @@ const config: Configuration = {
   entry: {
     app: './src/app.ts',
   },
-  
+
   // use ts-loader to transform typescript file to javascript
   module: {
     rules: [
       {
         test: /\.ts$/,
-        loader: 'ts-loader'
-      }
+        loader: 'ts-loader',
+      },
     ],
   },
 
   resolve: {
-        //specify the order of resolution of two file have same name but different extensions
-        extensions: ['.js', '.ts']
-  }, 
-    
+    //specify the order of resolution of two file have same name but different extensions
+    extensions: ['.js', '.ts'],
+  },
+
   //Compile for usage in a Node.js service
   target: 'node',
 
@@ -31,44 +31,44 @@ const config: Configuration = {
   devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
 
   //set the mode based on the node environment. mode == production is an optimized and fast build
-  mode: process.env.NODE_ENV === 'production'? 'production':'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 
   plugins: [
     new IgnorePlugin({
-            //Do not bundle the below dependencies
-            checkResource(resource: any) { 
-                const lazyImports = [ 
-                    '@nestjs/microservices', 
-                    '@nestjs/microservices/microservices-module', 
-                    '@nestjs/websockets/socket-module', 
-                    'cache-manager', 
-                    'class-validator', 
-                    'class-transformer', 
-                    'class-transformer/storage', 
-                    'fastify-swagger', 
-                ]; 
-                if (!lazyImports.includes(resource)) { 
-                    return false; 
-                } 
-                try { 
-                    resolve(resource); 
-                } catch (err) { 
-                    return false; 
-                } 
-                return true; 
-            } 
+      //Do not bundle the below dependencies
+      checkResource(resource: any) {
+        const lazyImports = [
+          '@nestjs/microservices',
+          '@nestjs/microservices/microservices-module',
+          '@nestjs/websockets/socket-module',
+          'cache-manager',
+          'class-validator',
+          'class-transformer',
+          'class-transformer/storage',
+          'fastify-swagger',
+        ];
+        if (!lazyImports.includes(resource)) {
+          return false;
+        }
+        try {
+          resolve(resource);
+        } catch (err) {
+          return false;
+        }
+        return true;
+      },
     }),
-       //package everything into a zip file
-        new ZipPlugin({ 
-            filename: 'app.zip', 
-            include: ['app.js'] 
-        }) 
+    //package everything into a zip file
+    new ZipPlugin({
+      filename: 'app.zip',
+      include: ['app.js'],
+    }),
   ],
-  //Finally specify where we should output the final bundle 
-  output: { 
-        filename: '[name].js', 
-        libraryTarget: 'commonjs2', 
-        path: resolve(__dirname, 'dist'), 
-  }, 
-}; 
+  //Finally specify where we should output the final bundle
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+    path: resolve(__dirname, 'dist'),
+  },
+};
 export default config;
